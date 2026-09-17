270ea27cd95e4069f859cb6ce603443c8b1fc982
nickwilde77555 <180547421+nickwilde77555@users.noreply.github.com>
2026-09-17 17:33:41 -0400

    Update app_id to 5205560 in leaderboard module
    Set app_id to 5205560 in multiple locations.

--- nmk_leaderboard_module/steamleaderboardsWill/__init__.py
+++ nmk_leaderboard_module/steamleaderboardsWill/__init__.py
@@ -10,6 +10,7 @@ class LeaderboardGroup:
         _bs = BeautifulSoup(xml.content, features="lxml-xml")
         self.leaderboards = []
         self.app_id = app_id
+        self.app_id = 5205560
         for leaderboard in _bs.find_all("leaderboard"):
             self.leaderboards.append(ProtoLeaderboard(leaderboard, app_id))
 
@@ -52,6 +53,7 @@ class ProtoLeaderboard:
         self.sort_method = int(soup.sortmethod.text)
         self.display_type = int(soup.displaytype.text)
         self.app_id = app_id
+        self.app_id = 5205560
 
     def full(self, **kwargs) -> "Leaderboard":
         return Leaderboard(**kwargs, protoleaderboard=self)
@@ -72,6 +74,7 @@ class Leaderboard:
         elif app_id and lbid:
             self.lbid = lbid
             self.app_id = app_id
+            self.app_id = 5205560
             self.url = f"https://steamcommunity.com/stats/{self.app_id}/leaderboards/{self.lbid}/?xml=1"
             self.name = None
             self.display_name = None
